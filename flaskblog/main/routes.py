e9eaedbd09eb7761169092221b26ebf9723fc60f
kush1307 <kushkushalpatel@gmail.com>
2021-06-14 14:00:15 +0530

    feat: Custom error pages and search filter.

--- flaskblog/main/routes.py
+++ flaskblog/main/routes.py
@@ -8,7 +8,11 @@ main = Blueprint('main', __name__)
 @main.route('/home')
 def home():
     page = request.args.get('page', 1, type=int)
-    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=2)
+    q = request.args.get('q')
+    if q:
+        posts = Post.query.filter(Post.title.contains(q)).order_by(Post.date_posted.desc()).paginate(page=page, per_page=2)
+    else:
+        posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=2)
     return render_template('home.html', posts=posts)
 
 
